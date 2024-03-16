1faf8735499c16856bac613be6a99bcea3f6774a
vagner ventura <vagnerventura@hotmail.com>
2024-03-16 10:43:15 -0300

    Insert Implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -18,13 +18,14 @@ public class Seller implements Serializable {
 	
 	public Seller () {}
 	
-	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
+		this.department = department;
 	}
 
 	public Integer getId() {
